640f969287f386b340901a9f62fb393f14427b2d
Vaibhavs2211 <vaibhavsaxena2002@gmail.com>
2026-04-17 17:17:27 +0530

    Project

---
+++ src/main/java/com/example/demo/services/UserServices.java
@@ -0,0 +1,69 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.entities.Admin;
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
+
+@Component
+public class UserServices 
+{
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> getAllUser()
+	{
+		List<User> users = (List<User>) this.userRepository.findAll();
+		return users;
+	}
+
+	public User getUser(int id)
+	{
+		Optional<User> optional = this.userRepository.findById(id);
+		User user = optional.get();
+		return user;
+	}
+	public User getUserByEmail(String email)
+	{
+	 User user=	this.userRepository.findUserByUemail(email);
+	 return user;
+	}
+
+	public void updateUser(User user,int id)
+	{
+		user.setU_id(id);
+		 this.userRepository.save(user);
+	}
+
+	public void deleteUser(int id)
+	{
+		this.userRepository.deleteById(id);
+	}
+
+	public void addUser(User user)
+	{
+	this.userRepository.save(user);
+        user.setUpassword(passwordEncoder.encode(user.getUpassword()));
+	}
+	
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+
+    public boolean validateLoginCredentials(String email, String password) {
+        User user = this.userRepository.findUserByUemail(email);
+        if (user != null && passwordEncoder.matches(password, user.getUpassword())) {
+            return true;
+        }
+        return false;
+    }
+
+}
